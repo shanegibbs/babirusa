c512d4a8bb034f1f1c98fe1ca9364f2c4fb46995
Shane Gibbs <shane@hands.net.nz>
2014-09-06 14:30:24 +1200

    extract common lib

--- src/lib/berkeley_registry/berkeley_registry_test.c
+++ src/lib/berkeley_registry/berkeley_registry_test.c
@@ -3,9 +3,22 @@
 
 #include "berkeley_registry.h"
 
+void berkely_registry_add_test(void)
+{
+  Registry *reg = BerkeleyRegistry()->new(NULL);
+  g_assert(reg != NULL);
+
+  Checksum *checksum = (Checksum*)"123abc";
+
+  Info *info = bab_info_new("myfile", 100, 250, checksum);
+
+  reg->add(reg, info);
+
+}
+
 int main(int argc, char** argv)
 {
-    g_test_init(&argc, &argv, NULL);
-    // g_test_add_func("/options/path", optionsPathTest);
-    return g_test_run();
+  g_test_init(&argc, &argv, NULL);
+  g_test_add_func("/berkely_registry/add", berkely_registry_add_test);
+  return g_test_run();
 }
